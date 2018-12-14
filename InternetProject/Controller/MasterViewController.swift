1f5386240c2771b82d64117902ae0997802daafd
Emilysavannahjames <43076722+Emilysavannahjames@users.noreply.github.com>
2018-12-14 13:26:27 -0700

    added a new viewcontroller and internet topics

---
+++ InternetProject/Controller/MasterViewController.swift
@@ -0,0 +1,70 @@
+//
+//  MasterViewController.swift
+//  InternetProject
+//
+//  Created by James, Emily on 12/14/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class MasterViewController: UITableViewController
+{
+    private weak var internetDetail : InternetDetailViewController? = nil
+    private lazy var internetTopics : [String] = [String]()
+    private lazy var addresses : [String] = [String]()
+    
+    private func setupDetailContents() -> Void
+    {
+        internetTopics = [
+        "Internet Definitions 🤠",
+        "Standard Search Engine 🐎",
+        "AP CSP 👷🏼‍♀️",
+        "Canyons District 🏫",
+        "CTEC 🏢",
+        "Social Media 📲"
+        ]
+        
+        addresses = [
+            "https://www.google.com",
+            "https://www.google.com",
+            "https://www.google.com",
+            "https://www.google.com",
+            "https://www.google.com",
+            "https://www.google.com"
+        ]
+        
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            internetDetail = currentControllers[0] as? InternetDetailViewController
+        }
+    }
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        
+        setupDetailContents()
+        self.clearsSelectionOnViewWillAppear = false
+        
+        if let split = splitViewController
+        {
+            let controllers = split.viewControllers
+            internetDetail = (controllers[controllers.count-1] as! UINavigationController).topViewController as? InternetDetailViewController
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
